3bff8216251cf03034d0e9f79e7e86bb568ea845
Jiaowei <wei.jiao@guanwu.com>
2020-09-02 20:01:06 +0800

    Add Landing Page Template

---
+++ _nuxt/layouts/default.1bb88fc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{683:function(n,t,e){"use strict";e.r(t);var l=e(1),component=Object(l.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("nuxt")],1)}),[],!1,null,null,null);t.default=component.exports}}]);
